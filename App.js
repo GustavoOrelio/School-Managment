8c0fe2204b1f44e4a38f5604e342de5b19a0d26c
Gustavo Orelio <oreliogustavo@gmail.com>
2022-06-20 20:38:58 -0300

    alterações na api

--- App.js
+++ App.js
@@ -6,10 +6,10 @@ const cors = require("cors");
 class App {
   static async init() {
     let app = new express();
-    app.use(express.json);
+    app.use(express.json());
     app.use(cors());
 
-    app.get("/endpoint", (req, res) => {
+    app.get("/", (req, res) => {
       res.json({
         name: "escola-api",
         version: "1.0.0",
@@ -17,6 +17,19 @@ class App {
         author: "Gustavo Orelio",
       });
     });
+    app.get("/ping", (req, res) => {
+      res.json({"Resposta": "pong"});
+    });
+
+    app.get("/contador", (req, res) => {
+      res.json({"Contador": "0"});
+    });
+
+    app.get("/incremento", (req, res) => {
+      contador ++;
+      res.json({"Contador": contador});
+    });
+
     app.listen(3000);
   }
 }
